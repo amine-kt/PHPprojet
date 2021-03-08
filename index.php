1f37f56b29ecf4d7bb8789df56717e6b7a911305
amine-kt <amine.tifour.1973@gmail.com>
2021-03-08 15:04:02 +0100

    add login page

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>
+            Yokuso Watakshi no Velvet Room
+        </h1>
+    </header>
+
+    <main>
+        <section>
+            <form action="" method="post">
+                <label for="">Login</label><br>
+                <input type="text" name="Login" placeholder="Login..."><br>
+                <label for="">Password</label><br>
+                <input type="text" name="Pasword" placeholder="Password..."><br>
+                <input type="submit" value="Connexion" class="button">
+            </form>
+        </section>
+    </main>
+
+
+</body>
+
+</html>
